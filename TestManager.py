b6790333b4d1aa1860aa6f695307c60113768657
tuanngokien <tuanngokien@gmail.com>
2020-03-19 22:03:25 +0700

    generate project config.report (with test output and spectrum & stmts coverage)

--- TestManager.py
+++ TestManager.py
@@ -17,14 +17,13 @@ JUNIT_PLUGIN_PATH = get_plugin_path(JUNIT_PLUGIN_NAME)
 
 def generate_junit_test_cases(variant_dir):
     logger.info(f"Generating JUnit Test for variant [{get_file_name(variant_dir)}]")
-    delete_dir("./.evosuite")
     compiled_classes_dir = get_compiled_source_classes_dir(variant_dir)
     test_cases_dir = get_test_dir(variant_dir)
     output_log = execute_shell_command(f'java -jar {EVOSUITE_PLUGIN_PATH}', extra_args=[
         {"-projectCP": compiled_classes_dir},
         {"-seed": 1583738192420},
         {"-target": compiled_classes_dir},
-        {"-continuous": "execute"},
+        {"-continuous": "clean"},
         {"-Dctg_memory": "4000"},
         {"-Dctg_cores": "4"},
         {"-Dctg_export_folder": test_cases_dir},
