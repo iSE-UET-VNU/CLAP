23f8275b81360658b994a62196dd358beef3dae3
tuanngokien <tuanngokien@gmail.com>
2020-06-08 20:31:43 +0700

    fix find interaction bug - add more output info

--- Main_MutatedProject_FindInteractionBugProj.py
+++ Main_MutatedProject_FindInteractionBugProj.py
@@ -1,6 +1,7 @@
 import MutantManager
 
-from FileManager import get_project_dir
+from FileManager import get_project_dir, get_project_name, get_file_name
+from SuspiciousStatementManager import get_buggy_statement
 
 if __name__ == "__main__":
     project_name = "3wise-Mutated-GPL-Test"
@@ -8,6 +9,8 @@ if __name__ == "__main__":
 
     mutated_project_dirs = MutantManager.get_mutated_project_dirs(project_dir)
     for mutated_project_dir in mutated_project_dirs:
+        mutated_project_name = get_project_name(mutated_project_dir)
         is_interaction_bug = MutantManager.check_interaction_bug_from_report(mutated_project_dir)
         if is_interaction_bug:
-            print(mutated_project_dir)
+            print(
+                f"{get_file_name(mutated_project_dir)} - [{get_buggy_statement(mutated_project_name, mutated_project_dir)}]")
