a74ba6ed90e596d2042fcbc46795b4556e146a2e
tuanngokien <tuanngokien@gmail.com>
2020-07-01 17:01:12 +0700

    add project ExamDB-FH-JML

--- VariantComposer.py
+++ VariantComposer.py
@@ -6,7 +6,7 @@ from Helpers import get_logger, execute_shell_command
 
 logger = get_logger(__name__)
 
-PLUGIN_NAME = "featurehouse_2020_constructor_concat.jar"
+PLUGIN_NAME = "featurehouse_20200701_constructor.jar"
 PLUGIN_PATH = get_plugin_path(PLUGIN_NAME)
 
 
