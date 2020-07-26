73c4ef1ce2fbab8fc7ebc8966dd1dbab0b3307b1
trangnt57 <nguyenthutrang.uet@gmail.com>
2020-07-26 14:52:15 +0700

    delete unnecessary files

--- Main_SummaryExperimentResult.py
+++
@@ -1,7 +0,0 @@
-from ExperimentResultManager import summary_experiment_result
-from FileManager import get_experimental_result_file
-
-if __name__ == "__main__":
-    result_file_name = "4wise-Mutated-GPL-Test_0.5_result.xlsx"
-    result_file_path = get_experimental_result_file(result_file_name)
-    summary_experiment_result(result_file_path)
