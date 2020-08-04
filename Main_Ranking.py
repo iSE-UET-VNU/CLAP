187e6ae3c3473c1b63f4d7f00c9a7124fe3dc0cf
huent189 <hueahgase189@gmail.com>
2020-08-04 13:18:02 +0700

    delete old experimental results

--- Main_Ranking.py
+++ Main_Ranking.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     system_name = "Elevator_Test"
     project_names = ["4wise"]
 
-    filtering_coverage_rate_list = [0.95]
+    filtering_coverage_rate_list = [0.5, 0.8, 0.95]
     for coverage_index in range(0, len(filtering_coverage_rate_list)):
          for project_index in range(0, len(project_names)):
             #ranking_with_coverage_rate(base_dir, project_names[project_index], filtering_coverage_rate_list[coverage_index], [TARANTULA, OCHIAI, OP2, BARINEL, DSTAR])
