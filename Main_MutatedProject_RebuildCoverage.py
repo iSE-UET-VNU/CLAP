95e23e241ca903ac2fc414b1a7ff52559f98f1dc
trangnt57 <nguyenthutrang.uet@gmail.com>
2020-09-30 14:06:40 +0700

    rebute test coverage for multiple variants

--- Main_MutatedProject_RebuildCoverage.py
+++ Main_MutatedProject_RebuildCoverage.py
@@ -1,15 +1,19 @@
 import CoverageManager
 import MutantManager
-from FileManager import get_project_dir
+from FileManager import get_project_dir, get_mutated_projects_dir, list_dir
 from Helpers import get_version_by_time
 
 if __name__ == "__main__":
     # version = "20200928_011024"
-    version = get_version_by_time()
-    base_dir = "/Users/tuanngokien/Desktop/Software_Analysis/configurable_system/InputPreparation/projects/"
-    project_name = "Elevator-FH-JML"
-    project_dir = get_project_dir(project_name, base_dir)
-    mutated_project_name = "Empty.ElevatorSystem.Environment.ROR_1"
-    mutated_project_dir = MutantManager.get_mutated_project_dir(project_dir, mutated_project_name)
-    spc_log_file_path = CoverageManager.rebuild_spectrum_coverage_for_mutated_project(mutated_project_dir,
-                                                                                      version=version)
+    version = "v2"
+    base_dir = "/Users/thu-trangnguyen/Documents/Research/configurable_system/Experiment/projects/Email/"
+    project_names = ["4wise"]
+    for project in project_names:
+        project_dir = get_project_dir(project, base_dir)
+        mutated_projects_dir = get_mutated_projects_dir(project_dir)
+        mutated_projects = list_dir(mutated_projects_dir)
+        for mutated_project_name in mutated_projects:
+            #mutated_project_name = "AutoResponder.EmailSystem.Client.COI_2"
+            mutated_project_dir = MutantManager.get_mutated_project_dir(project_dir, mutated_project_name)
+            spc_log_file_path = CoverageManager.rebuild_spectrum_coverage_for_mutated_project(mutated_project_dir,
+                                                                                          version=version)
