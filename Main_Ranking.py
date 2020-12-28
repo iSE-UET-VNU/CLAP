f56d2a3d9ee6d521ff86b8467621b414b4fd123a
huent189 <hueahgase189@gmail.com>
2020-12-29 00:08:08 +0700

    update experimental results for non-normalization

--- Main_Ranking.py
+++ Main_Ranking.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
     system_names = ["GPL"]
     project_names = ["4wise"]
 
-    spectrum_coverage_prefix = "v99__"
+    spectrum_coverage_prefix = "20__"
 
-    filtering_coverage_rate_list = [0.95]
+    filtering_coverage_rate_list = [0.0]
     for coverage_index in range(0, len(filtering_coverage_rate_list)):
         for system in system_names:
              system_dir = join_path(base_dir, system)
@@ -26,19 +26,19 @@ if __name__ == "__main__":
                  print(k_wise_dir)
                  if os.path.isdir(k_wise_dir):
                     #ranking_with_coverage_rate(base_dir, project_names[project_index], filtering_coverage_rate_list[coverage_index], [TARANTULA, OCHIAI, OP2, BARINEL, DSTAR])
-                    ranking_with_coverage_rate(system_dir, system, project_names[project_index],
-                                               filtering_coverage_rate_list[coverage_index],
-                                               [TARANTULA, OCHIAI, OP2, BARINEL, DSTAR,
-                                                RUSSELL_RAO, SIMPLE_MATCHING, ROGERS_TANIMOTO, AMPLE, JACCARD,
-                                                COHEN, SCOTT, ROGOT1, GEOMETRIC_MEAN, M2,
-                                                WONG1, SOKAL, SORENSEN_DICE, DICE, HUMANN,
-                                                M1, WONG2, WONG3, ZOLTAR, OVERLAP,
-                                                EUCLID, ROGOT2, HAMMING, FLEISS, ANDERBERG,
-                                                GOODMAN, HARMONIC_MEAN, KULCZYNSKI1, KULCZYNSKI2],
-                                               spectrum_coverage_prefix)
                     # ranking_with_coverage_rate(system_dir, system, project_names[project_index],
-                    #                           filtering_coverage_rate_list[coverage_index],
-                    #                           [TARANTULA, OCHIAI, OP2, BARINEL, DSTAR], spectrum_coverage_prefix)
+                    #                            filtering_coverage_rate_list[coverage_index],
+                    #                            [TARANTULA, OCHIAI, OP2, BARINEL, DSTAR,
+                    #                             RUSSELL_RAO, SIMPLE_MATCHING, ROGERS_TANIMOTO, AMPLE, JACCARD,
+                    #                             COHEN, SCOTT, ROGOT1, GEOMETRIC_MEAN, M2,
+                    #                             WONG1, SOKAL, SORENSEN_DICE, DICE, HUMANN,
+                    #                             M1, WONG2, WONG3, ZOLTAR, OVERLAP,
+                    #                             EUCLID, ROGOT2, HAMMING, FLEISS, ANDERBERG,
+                    #                             GOODMAN, HARMONIC_MEAN, KULCZYNSKI1, KULCZYNSKI2],
+                    #                            spectrum_coverage_prefix)
+                    ranking_with_coverage_rate(system_dir, system, project_names[project_index],
+                                              filtering_coverage_rate_list[coverage_index],
+                                              [TARANTULA, OCHIAI, OP2, BARINEL, DSTAR], spectrum_coverage_prefix)
 
 
 
--- SuspiciousStatementManager.py
+++ SuspiciousStatementManager.py
@@ -9,6 +9,7 @@ import xml.etree.ElementTree as ET
 
 def get_suspicious_statement(mutated_project_dir, filtering_coverage_rate):
     slicing_info_file_path = get_slicing_log_file_path(mutated_project_dir, filtering_coverage_rate)
+    print(slicing_info_file_path)
     failing_coverage_data = read_coverage_file(mutated_project_dir)
     if (isfile(slicing_info_file_path)):
         slicing_info_file = open(slicing_info_file_path, "r")
