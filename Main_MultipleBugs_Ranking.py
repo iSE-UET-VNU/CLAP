cc54aa604d4a4b3988db33a8911a1bbe7c27a588
ttrangnguyen <nguyenthutrang.uet@gmail.com>
2021-05-02 14:32:32 +0700

    add kwise parameter when ranking

--- Main_MultipleBugs_Ranking.py
+++ Main_MultipleBugs_Ranking.py
@@ -7,12 +7,13 @@ from ranking.Spectrum_Expression import JACCARD, SORENSEN_DICE, TARANTULA, OCHIA
 
 
 if __name__ == "__main__":
-    base_folder = "DataV2_K"
-    base_dir = "/home/huent/Documents/Trang/VarCop/"
-    system_names = ["ExamDB"]
+    base_folder = "temp"
+    base_dir = "/Users/thu-trangnguyen/Documents/Data/VarCop/"
+    system_names = ["BankAccountTP"]
     bug_folders = ["1Bug"]
-    #alpha = [0.5, 0]
-    alpha = [1, 0.3, 0.7]
+    alpha = [1]
+    kwise_list = ["5wise"]
+    #alpha = [1, 0.3, 0.7]
     filtering_coverage_rate_list = [0.0]
     for k in alpha:
         result_folder = base_folder + str(k)
@@ -20,21 +21,22 @@ if __name__ == "__main__":
             system_dir = join_path(base_dir, system_name)
             for bug_folder in bug_folders:
                 bug_folder_dir = join_path(system_dir, bug_folder)
-                for coverage in filtering_coverage_rate_list:
-                    print(bug_folder_dir)
-                    # mutiple_bugs_ranking(result_folder, system_name, bug_folder, bug_folder_dir,
-                    #                      [TARANTULA], coverage, alpha)
-                    mutiple_bugs_ranking(result_folder, system_name, bug_folder,  bug_folder_dir, [TARANTULA, OCHIAI, OP2, BARINEL, DSTAR,
-                                                            RUSSELL_RAO, SIMPLE_MATCHING, ROGERS_TANIMOTO, AMPLE, JACCARD,
-                                                            COHEN, SCOTT, ROGOT1, GEOMETRIC_MEAN, M2,
-                                                            WONG1, SOKAL, SORENSEN_DICE, DICE, HUMANN,
-                                                             WONG2, EUCLID, ZOLTAR,
-                                                             ROGOT2, HAMMING, FLEISS, ANDERBERG,
-                                                            GOODMAN, HARMONIC_MEAN, KULCZYNSKI2], coverage, k)
-                    # sbfl_only_mutiple_bugs_ranking(result_folder, system_name, bug_folder, bug_folder_dir, [TARANTULA, OCHIAI, OP2, BARINEL, DSTAR,
-                    #                                         RUSSELL_RAO, SIMPLE_MATCHING, ROGERS_TANIMOTO, AMPLE, JACCARD,
-                    #                                         COHEN, SCOTT, ROGOT1, GEOMETRIC_MEAN, M2,
-                    #                                         WONG1, SOKAL, SORENSEN_DICE, DICE, HUMANN,
-                    #                                          WONG2, EUCLID, ZOLTAR,
-                    #                                          ROGOT2, HAMMING, FLEISS, ANDERBERG,
-                    #                                         GOODMAN, HARMONIC_MEAN, KULCZYNSKI2], coverage)
+                for kwise in kwise_list:
+                    for coverage in filtering_coverage_rate_list:
+                        print(bug_folder_dir)
+                        # mutiple_bugs_ranking(result_folder, system_name, bug_folder,  bug_folder_dir,
+                        #                      [TARANTULA], coverage, k)
+                        mutiple_bugs_ranking(result_folder, system_name, bug_folder,  bug_folder_dir, kwise, [TARANTULA, OCHIAI, OP2, BARINEL, DSTAR,
+                                                                RUSSELL_RAO, SIMPLE_MATCHING, ROGERS_TANIMOTO, AMPLE, JACCARD,
+                                                                COHEN, SCOTT, ROGOT1, GEOMETRIC_MEAN, M2,
+                                                                WONG1, SOKAL, SORENSEN_DICE, DICE, HUMANN,
+                                                                 WONG2, EUCLID, ZOLTAR,
+                                                                 ROGOT2, HAMMING, FLEISS, ANDERBERG,
+                                                                GOODMAN, HARMONIC_MEAN, KULCZYNSKI2], coverage, k)
+                        # sbfl_only_mutiple_bugs_ranking(result_folder, system_name, bug_folder, bug_folder_dir, [TARANTULA, OCHIAI, OP2, BARINEL, DSTAR,
+                        #                                         RUSSELL_RAO, SIMPLE_MATCHING, ROGERS_TANIMOTO, AMPLE, JACCARD,
+                        #                                         COHEN, SCOTT, ROGOT1, GEOMETRIC_MEAN, M2,
+                        #                                         WONG1, SOKAL, SORENSEN_DICE, DICE, HUMANN,
+                        #                                          WONG2, EUCLID, ZOLTAR,
+                        #                                          ROGOT2, HAMMING, FLEISS, ANDERBERG,
+                        #                                         GOODMAN, HARMONIC_MEAN, KULCZYNSKI2], coverage)
--- ranking/MultipleBugsManager.py
+++ ranking/MultipleBugsManager.py
@@ -68,79 +68,81 @@ def write_result_to_file(row, sheet, ranking_results, space):
     return row
 
 
-def mutiple_bugs_ranking(result_folder, system_name, bug_folder, system_dir, spectrum_expressions, filtering_coverage_rate, alpha):
+def mutiple_bugs_ranking(result_folder, system_name, bug_folder, system_dir, kwise, spectrum_expressions, filtering_coverage_rate, alpha):
     aggregations = [RankingManager.AGGREGATION_ARITHMETIC_MEAN]
     normalizations = [RankingManager.NORMALIZATION_ALPHA_BETA]
-    mutated_projects_dir = ""
-    for folder_name in list_dir(system_dir):
-        mutated_projects_dir = get_project_sub_dir_by_folder_name(system_dir, folder_name)
-    mutated_projects = list_dir(mutated_projects_dir)
-
-    result_folder_dir = join_path(EXPERIMENT_RESULT_FOLDER, result_folder)
-    if not os.path.exists(result_folder_dir):
-        os.makedirs(result_folder_dir)
-
-    system_result_dir = join_path(result_folder_dir, system_name)
-    if not os.path.exists(system_result_dir):
-        os.makedirs(system_result_dir)
 
 
-    for normalization_type in normalizations:
-        normalization_result_dir = join_path(system_result_dir, normalization_type)
-        if not os.path.exists(normalization_result_dir):
-            os.makedirs(normalization_result_dir)
+    mutated_projects_dir = join_path(system_dir, kwise)
+    print(mutated_projects_dir)
+    if(os.path.exists(mutated_projects_dir)):
+        mutated_projects = list_dir(mutated_projects_dir)
 
-        for aggregation_type in aggregations:
-            aggregation_result_dir = join_path(normalization_result_dir, aggregation_type)
-            if not os.path.exists(aggregation_result_dir):
-                os.makedirs(aggregation_result_dir)
+        result_folder_dir = join_path(EXPERIMENT_RESULT_FOLDER, result_folder)
+        if not os.path.exists(result_folder_dir):
+            os.makedirs(result_folder_dir)
 
-            kwise_result_dir = join_path(aggregation_result_dir, "4wise")
-            if not os.path.exists(kwise_result_dir):
-                os.makedirs(kwise_result_dir)
+        system_result_dir = join_path(result_folder_dir, system_name)
+        if not os.path.exists(system_result_dir):
+            os.makedirs(system_result_dir)
 
-            sheet = []
 
-            row = 0
+        for normalization_type in normalizations:
+            normalization_result_dir = join_path(system_result_dir, normalization_type)
+            if not os.path.exists(normalization_result_dir):
+                os.makedirs(normalization_result_dir)
 
-            experiment_file_name = join_path(kwise_result_dir,
-                                             bug_folder + ".xlsx")
-            if os.path.exists(experiment_file_name):
-                continue
-            wb = Workbook(experiment_file_name)
+            for aggregation_type in aggregations:
+                aggregation_result_dir = join_path(normalization_result_dir, aggregation_type)
+                if not os.path.exists(aggregation_result_dir):
+                    os.makedirs(aggregation_result_dir)
 
-            for i in range(0, len(spectrum_expressions)):
-                sheet.append(wb.add_worksheet(spectrum_expressions[i]))
-                write_header_in_result_file(row, sheet[i])
-            row += 1
-            num_of_bugs = 0
-            runtime = {}
-            for mutated_project_name in mutated_projects:
-                num_of_bugs += 1
-                mutated_project_dir = join_path(mutated_projects_dir, mutated_project_name)
-                #spc_log_file_path, spc_runtime = SPCsManager.find_SPCs(mutated_project_dir, filtering_coverage_rate)
+                kwise_result_dir = join_path(aggregation_result_dir, kwise)
+                if not os.path.exists(kwise_result_dir):
+                    os.makedirs(kwise_result_dir)
 
-                #spc_log_file_path = get_spc_log_file_path(mutated_project_dir, filtering_coverage_rate)
-                #slicing_runtime = SlicingManager.do_slice(spc_log_file_path, filtering_coverage_rate, "")
-                suspicious_stms_list = get_suspicious_statement(mutated_project_dir, filtering_coverage_rate)
-                buggy_statements = get_multiple_buggy_statements(mutated_project_name, mutated_project_dir)
-                # print(buggy_statements)
-                #
+                sheet = []
 
-                row_temp = row
-                varcop_ranking_time = 0
-                for sbfl_expression in range(0, len(spectrum_expressions)):
-                    ranking_results, space, varcop_ranking_time = ranking_multiple_bugs(buggy_statements, mutated_project_dir,
-                                                                   suspicious_stms_list,
-                                                                   spectrum_expressions[sbfl_expression],
-                                                                   aggregation_type,
-                                                                   normalization_type, "", filtering_coverage_rate, alpha)
-                    ranking_results[FB_RANK] = features_ranking_multiple_bugs(buggy_statements, mutated_project_dir,
-                                                            filtering_coverage_rate, spectrum_expressions[sbfl_expression])
+                row = 0
+
+                experiment_file_name = join_path(kwise_result_dir,
+                                                 bug_folder + ".xlsx")
+                if os.path.exists(experiment_file_name):
+                    continue
+                wb = Workbook(experiment_file_name)
+
+                for i in range(0, len(spectrum_expressions)):
+                    sheet.append(wb.add_worksheet(spectrum_expressions[i]))
+                    write_header_in_result_file(row, sheet[i])
+                row += 1
+                num_of_bugs = 0
+                runtime = {}
+                for mutated_project_name in mutated_projects:
+                    num_of_bugs += 1
+                    mutated_project_dir = join_path(mutated_projects_dir, mutated_project_name)
+                    #spc_log_file_path, spc_runtime = SPCsManager.find_SPCs(mutated_project_dir, filtering_coverage_rate)
+
+                    #spc_log_file_path = get_spc_log_file_path(mutated_project_dir, filtering_coverage_rate)
+                    #slicing_runtime = SlicingManager.do_slice(spc_log_file_path, filtering_coverage_rate, "")
+                    suspicious_stms_list = get_suspicious_statement(mutated_project_dir, filtering_coverage_rate)
+                    buggy_statements = get_multiple_buggy_statements(mutated_project_name, mutated_project_dir)
+                    # print(buggy_statements)
                     #
-                    sheet[sbfl_expression].write(row_temp, BUG_ID_COL, mutated_project_name)
-                    row = write_result_to_file(row_temp, sheet[sbfl_expression], ranking_results, space)
-                #runtime[mutated_project_name] = [spc_runtime, slicing_runtime, varcop_ranking_time]
-            #write_runtime_to_file(system_result_dir, runtime, "multiple_bug_runtime.xlsx")
 
-            wb.close()
+                    row_temp = row
+                    varcop_ranking_time = 0
+                    for sbfl_expression in range(0, len(spectrum_expressions)):
+                        ranking_results, space, varcop_ranking_time = ranking_multiple_bugs(buggy_statements, mutated_project_dir,
+                                                                       suspicious_stms_list,
+                                                                       spectrum_expressions[sbfl_expression],
+                                                                       aggregation_type,
+                                                                       normalization_type, "", filtering_coverage_rate, alpha)
+                        ranking_results[FB_RANK] = features_ranking_multiple_bugs(buggy_statements, mutated_project_dir,
+                                                                filtering_coverage_rate, spectrum_expressions[sbfl_expression])
+                        #
+                        sheet[sbfl_expression].write(row_temp, BUG_ID_COL, mutated_project_name)
+                        row = write_result_to_file(row_temp, sheet[sbfl_expression], ranking_results, space)
+                    #runtime[mutated_project_name] = [spc_runtime, slicing_runtime, varcop_ranking_time]
+                #write_runtime_to_file(system_result_dir, runtime, "multiple_bug_runtime.xlsx")
+
+                wb.close()
