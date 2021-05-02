3b1cfcbfc7aa47e4d128199585e8794c6caef138
ttrangnguyen <nguyenthutrang.uet@gmail.com>
2021-05-02 17:43:48 +0700

    percentage of bugs/cases found

--- experimental_results_analyzer/ExperimentalResultsAnalyzer.py
+++ experimental_results_analyzer/ExperimentalResultsAnalyzer.py
@@ -1,6 +1,5 @@
 import os
 
-
 import pandas
 
 from FileManager import join_path, EXPERIMENT_RESULT_FOLDER
@@ -15,7 +14,9 @@ from ranking.Spectrum_Expression import JACCARD, SORENSEN_DICE, TARANTULA, OCHIA
 
 from xlsxwriter import Workbook
 
-data_column = [VARCOP_RANK, VARCOP_EXAM, VARCOP_SPACE, VARCOP_DISABLE_BPC_RANK, VARCOP_DISABLE_BPC_EXAM, SBFL_RANK, SBFL_EXAM, FB_RANK, FB_EXAM, SPACE]
+data_column = [VARCOP_RANK, VARCOP_EXAM, VARCOP_SPACE, VARCOP_DISABLE_BPC_RANK, VARCOP_DISABLE_BPC_EXAM, SBFL_RANK,
+               SBFL_EXAM, FB_RANK, FB_EXAM, SPACE]
+rank_column = [VARCOP_RANK, VARCOP_DISABLE_BPC_RANK, SBFL_RANK, FB_RANK]
 SBFL_METRIC_COL = 0
 NUM_CASES_COL = 1
 NUM_BUGS_COL = 2
@@ -47,7 +48,6 @@ SPECTRUM_EXPRESSIONS_LIST = [TARANTULA, OCHIAI, OP2, BARINEL, DSTAR,
 # SPECTRUM_EXPRESSIONS_LIST = [TARANTULA]
 
 def write_all_bugs_to_a_file(summary_file_dir, file_lists, num_of_bugs, base_path):
-
     writer = pandas.ExcelWriter(summary_file_dir, engine='openpyxl')
 
     row = 0
@@ -61,12 +61,14 @@ def write_all_bugs_to_a_file(summary_file_dir, file_lists, num_of_bugs, base_pat
                 num_of_file += 1
                 if num_of_file > 1:
                     for spectrum_expression_type in SPECTRUM_EXPRESSIONS_LIST:
-                        excel_data_df[spectrum_expression_type].to_excel(writer, sheet_name=spectrum_expression_type, startrow=row,
+                        excel_data_df[spectrum_expression_type].to_excel(writer, sheet_name=spectrum_expression_type,
+                                                                         startrow=row,
                                                                          index=False, header=False)
                     row += len(excel_data_df[TARANTULA])
                 else:
                     for spectrum_expression_type in SPECTRUM_EXPRESSIONS_LIST:
-                        excel_data_df[spectrum_expression_type].to_excel(writer, sheet_name=spectrum_expression_type, startrow=row,
+                        excel_data_df[spectrum_expression_type].to_excel(writer, sheet_name=spectrum_expression_type,
+                                                                         startrow=row,
                                                                          index=False)
                     row += len(excel_data_df[TARANTULA]) + 1
 
@@ -182,6 +184,7 @@ def calculate_average_in_a_file(experimental_file_dir, row, sheet):
         sheet.write(row, SBFL_METRIC_COL, spectrum_expression_type)
 
         average_value_list = average_best_rank_exam(excel_data_df, spectrum_expression_type)
+        #percentage_of_cases_found_bugs(experimental_file_dir, spectrum_expression_type, 3)
         col = VARCOP_RANK_COL
         for metric in data_column:
             sheet.write(row, col, average_value_list[metric])
@@ -192,65 +195,59 @@ def calculate_average_in_a_file(experimental_file_dir, row, sheet):
     return row
 
 
-
-def count_total_bugs(files_list):
-    bug_list = []
-    overall_info = {}
-
-    for file in files_list.keys():
-        excel_data = pandas.read_excel(files_list[file], sheet_name=None)
-        overall_info[file] = excel_data
-
-    for file in overall_info.keys():
-        for bug in overall_info[file][TARANTULA][BUG_ID]:
-            if bug not in bug_list:
-                print(bug)
-                bug_list.append(bug)
-    return len(bug_list)
-
 MAX = 100000
 MIN = -100000
 
 
+def percentage_of_bugs_found_per_case(experimental_file_dir, sbfl_metric, num_examined_stm):
+    excel_data_df = pandas.read_excel(experimental_file_dir, sheet_name=None)
+    index = 0
+    percentage_list = {}
+    for metric in rank_column:
+        percentage_list[metric] = []
+    while index < len(excel_data_df[sbfl_metric][BUG_ID]):
+        data, index = get_values_of_a_case(excel_data_df, sbfl_metric, index, rank_column)
+        for metric in rank_column:
+            temp = 0
+            for item in data[metric]:
+                if item <= num_examined_stm:
+                    temp += 1
+            percentage_list[metric].append(temp / len(data[metric]))
 
-def percentage_of_bugs_found(multiple_bugs_file, sbfl_metric, examined_statements):
-    excel_data_df = pandas.read_excel(multiple_bugs_file, sheet_name=None)
-    results = {}
-    for num in examined_statements:
-        sum_varcop = 0
-        sum_sbfl = 0
-        num_of_cases = 0
-        count = 0
-        for i in range(0, len(excel_data_df[sbfl_metric][BUG_ID])):
-            if (excel_data_df[sbfl_metric][BUG_ID][i] != BUG_ID and not pandas.isnull(
-                    excel_data_df[sbfl_metric][BUG_ID][i])):
-                if (count != 0):
-                    sum_varcop += varcop / count
-                    sum_sbfl += sbpl / count
-                    num_of_cases += 1
-                varcop = 0
-                sbpl = 0
-                count = 0
+    average_percentage_list = {}
+    for metric in rank_column:
+        average_percentage_list[metric] = calculate_average(percentage_list[metric])
+    return average_percentage_list
 
-            varcop_rank = excel_data_df[sbfl_metric]["VARCOP:RANK"][i]
-            sbfl_rank = excel_data_df[sbfl_metric]["SBFL:RANK"][i]
 
-            if (type(varcop_rank) == int and varcop_rank != -1 and varcop_rank <= num):
-                varcop += 1
-            if (type(sbfl_rank) == int and sbfl_rank != -1 and sbfl_rank <= num):
-                sbpl += 1
-            count += 1
-        results[num] = {}
-        results[num]["varcop"] = sum_varcop / num_of_cases
-        results[num]["sbfl"] = sum_sbfl / num_of_cases
-    return results
+def percentage_of_cases_found_bugs(experimental_file_dir, sbfl_metric, num_examined_stm):
+    excel_data_df = pandas.read_excel(experimental_file_dir, sheet_name=None)
+    index = 0
+    percentage_list = {}
+    for metric in rank_column:
+        percentage_list[metric] = 0
+    while index < len(excel_data_df[sbfl_metric][BUG_ID]):
+        data, index = get_values_of_a_case(excel_data_df, sbfl_metric, index, rank_column)
+        for metric in rank_column:
+            for item in data[metric]:
+                if item <= num_examined_stm:
+                    percentage_list[metric] += 1
+                    break
+
+    average_percentage_list = {}
+    num_of_cases = num_of_element(excel_data_df[sbfl_metric][BUG_ID])
+    for metric in rank_column:
+        average_percentage_list[metric] = percentage_list[metric]/num_of_cases
+    print(average_percentage_list)
+    return average_percentage_list
 
 
 def calculate_average(data):
     sum = 0
     for item in data:
         sum += item
-    return sum/len(data)
+    return sum / len(data)
+
 
 def average_best_rank_exam(excel_data_df, sbfl_metric):
     best_value_list = get_best_rank_exam(excel_data_df, sbfl_metric)
@@ -259,12 +256,14 @@ def average_best_rank_exam(excel_data_df, sbfl_metric):
         average_value_list[metric] = calculate_average(best_value_list[metric])
     return average_value_list
 
+
 def average_worst_rank_exam(excel_data_df, sbfl_metric):
     best_value_list = get_worst_rank_exam(excel_data_df, sbfl_metric)
     average_value_list = {}
     for metric in data_column:
         average_value_list[metric] = calculate_average(best_value_list[metric])
     return average_value_list
+
 
 def get_best_rank_exam(excel_data_df, sbfl_metric):
     index = 0
@@ -273,10 +272,11 @@ def get_best_rank_exam(excel_data_df, sbfl_metric):
         best_value_list[metric] = []
 
     while index < len(excel_data_df[sbfl_metric][BUG_ID]):
-        data, index = get_values_of_a_case(excel_data_df, sbfl_metric, index)
+        data, index = get_values_of_a_case(excel_data_df, sbfl_metric, index, data_column)
         for metric in data_column:
             best_value_list[metric].append(min(data[metric]))
     return best_value_list
+
 
 def get_worst_rank_exam(excel_data_df, sbfl_metric):
     index = 0
@@ -285,20 +285,20 @@ def get_worst_rank_exam(excel_data_df, sbfl_metric):
         best_value_list[metric] = []
 
     while index < len(excel_data_df[sbfl_metric][BUG_ID]):
-        data, index = get_values_of_a_case(excel_data_df, sbfl_metric, index)
+        data, index = get_values_of_a_case(excel_data_df, sbfl_metric, index, data_column)
         for metric in data_column:
             best_value_list[metric].append(max(data[metric]))
     return best_value_list
 
-def get_values_of_a_case(excel_data_df, sbfl_metric, index):
-    data = {}
-    for metric in data_column:
-        data[metric] = []
-        data[metric].append(excel_data_df[sbfl_metric][metric][index])
+
+def get_values_of_a_case(excel_data_df, sbfl_metric, index, column_list):
+    list_of_values = {}
+    for metric in column_list:
+        list_of_values[metric] = []
+        list_of_values[metric].append(excel_data_df[sbfl_metric][metric][index])
     index += 1
     while index < len(excel_data_df[sbfl_metric][BUG_ID]) and pandas.isnull(excel_data_df[sbfl_metric][BUG_ID][index]):
-        for metric in data_column:
-            data[metric].append(excel_data_df[sbfl_metric][metric][index])
+        for metric in column_list:
+            list_of_values[metric].append(excel_data_df[sbfl_metric][metric][index])
         index += 1
-    return data, index
-
+    return list_of_values, index
