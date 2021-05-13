16120fc9b4f0cad361088534751824466f5d62f5
tuanngokien <tuanngokien@gmail.com>
2021-05-13 18:48:44 +0700

    [Slicing] Main_RunForSPCAndSlicing.py for multiple coverages

--- Main_RunForSPCAndSlicing.py
+++ Main_RunForSPCAndSlicing.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
     mutants_dir = "/home/huent/Documents/Trang/Debug/ExamDB/1Bug/4wise/"
     mutated_project_dirs = list_dir(mutants_dir, full_path=True)
     coverage = 0.0
-    coverage_verions = ["INoT_1_", "INoT_2_", "INoT_3_", "INoT_4_", "INoT_5_", "INoT_6_", "INoT_7_", "INoT_8_",
+    coverage_versions = ["INoT_1_", "INoT_2_", "INoT_3_", "INoT_4_", "INoT_5_", "INoT_6_", "INoT_7_", "INoT_8_",
                         "INoT_9_", "INoT_10_"]
     for mutated_project_dir in mutated_project_dirs:
-        for version in coverage_verions:
-            try:
-                lock_project(mutated_project_dir)
-            except BlockingIOError as e:
-                continue
+        try:
+            lock_project(mutated_project_dir)
+        except BlockingIOError as e:
+            continue
+        for version in coverage_versions:
             # SPCsManager.find_SPCs(mutated_project_dir, coverage)
             spc_log_file_path = get_spc_log_file_path(mutated_project_dir, coverage)
             slicing_runtime = SlicingManager.do_slice(spc_log_file_path, coverage, version)
