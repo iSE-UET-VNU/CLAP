68183ad30b79025024b86c5dd58b93f735745eb6
trangnt57 <hueahgase189@gmail.com>
2021-05-13 17:44:45 +0700

    Script for slicing with multiple coverage versions

--- Main_RunForSPCAndSlicing.py
+++ Main_RunForSPCAndSlicing.py
@@ -3,14 +3,16 @@ from spc import SPCsManager
 from suspicious_statements_manager import SlicingManager
 
 if __name__ == "__main__":
-    mutants_dir = "/home/huent/Documents/Trang/temp/BankAccountTP/1Bug/mutated_projects"
+    mutants_dir = "/home/huent/Documents/Trang/Debug/ExamDB/1Bug/4wise/"
     mutated_project_dirs = list_dir(mutants_dir, full_path=True)
     coverage = 0.0
+    coverage_verions = ["INoT_1_", "INoT_2_"]
     for mutated_project_dir in mutated_project_dirs:
-        try:
-            lock_project(mutated_project_dir)
-        except BlockingIOError as e:
-            continue
-        SPCsManager.find_SPCs(mutated_project_dir, coverage)
-        spc_log_file_path = get_spc_log_file_path(mutated_project_dir, coverage)
-        slicing_runtime = SlicingManager.do_slice(spc_log_file_path, coverage, "")
+        for verion in coverage_verions:
+            # try:
+            #     lock_project(mutated_project_dir)
+            # except BlockingIOError as e:
+            #     continue
+            #SPCsManager.find_SPCs(mutated_project_dir, coverage)
+            spc_log_file_path = get_spc_log_file_path(mutated_project_dir, coverage)
+            slicing_runtime = SlicingManager.do_slice(spc_log_file_path, coverage, verion)
