604e62b40a6ab23db066c4d93b5fb0568a9603e1
trangnt57 <nguyenthutrang.uet@gmail.com>
2021-12-08 22:31:55 +0700

    Evaluate passing variant classification performance per system

--- Main_SVM_Classifier.py
+++ Main_SVM_Classifier.py
@@ -5,7 +5,7 @@ from sklearn import metrics
 from consistent_testing_manager.FPMatricsCaculation import *
 import numpy as np
 from consistent_testing_manager.FileName import classified_all_file, classified_all_single_bug_file, \
-    classified_all_multiple_bug_file
+    classified_all_multiple_bug_file, classified_all_testing_file
 
 # FEATURES = [DDU,
 #             not_executed_susp_stmt_vs_susp_stmt_in_passing_variant,
@@ -18,15 +18,14 @@ from consistent_testing_manager.FileName import classified_all_file, classified_
 
 
 FEATURES = [DDU,
-            not_executed_susp_stmt_vs_susp_stmt_in_passing_variant,
             executed_susp_stmt_vs_susp_stmt_in_a_failed_execution,
             not_executed_susp_stmt_vs_susp_stmt_in_a_failed_execution,
             tested_unexpected_behaviors_in_passing_variant + "_0.8",
-            confirmed_successes_in_passing_variant + "_0.8",
-            total_susp_scores_in_variants]
+            confirmed_successes_in_passing_variant + "_1.0",
+            total_susp_scores_in_system, both_forward_and_backward_similarity, forward_similarity, backward_similarity]
 
 
-def load_all_data(training_systems, testing_systems, system_ratio, variant_ratio = 0):
+def load_all_data(training_systems, testing_systems, system_ratio, variant_ratio=0):
     all_systems = []
     for s in training_systems:
         all_systems.append(s)
@@ -48,15 +47,16 @@ def load_all_data(training_systems, testing_systems, system_ratio, variant_ratio
         mutated_projects = list_dir(system)
         for project in mutated_projects:
             project_dir = join_path(system, project)
-            consistent_testing_info_normalized_file_path = join_path(project_dir, consistent_testing_normalized_info_file)
+            consistent_testing_info_normalized_file_path = join_path(project_dir,
+                                                                     consistent_testing_normalized_info_file)
             df = pandas.read_csv(consistent_testing_info_normalized_file_path)
             if system in training_systems or count < system_ratio * len(mutated_projects):
                 if variant_ratio == 0:
                     train_temp.append(df)
                 else:
-                    num_train_items = int(variant_ratio*df.shape[0])
+                    num_train_items = int(variant_ratio * df.shape[0])
                     df1 = df.iloc[:num_train_items, :]
-                    df2 = df.iloc[num_train_items+1:, :]
+                    df2 = df.iloc[num_train_items + 1:, :]
                     train_temp.append(df1)
                     test_temp.append(df2)
                     test_sample[system].append(project)
@@ -67,9 +67,9 @@ def load_all_data(training_systems, testing_systems, system_ratio, variant_ratio
                 if variant_ratio == 0:
                     test_temp.append(df)
                 else:
-                    num_train_items = int(variant_ratio*df.shape[0])
+                    num_train_items = int(variant_ratio * df.shape[0])
                     df1 = df.iloc[:num_train_items, :]
-                    df2 = df.iloc[num_train_items+1:, :]
+                    df2 = df.iloc[num_train_items + 1:, :]
                     train_temp.append(df1)
                     test_temp.append(df2)
                     training_system += 1
@@ -117,10 +117,10 @@ def write_classified_result(y_pred, test_samples, variant_ratio, file_name):
                 labels = df[LABEL]
                 label_index = 0
             else:
-                num_train_items = int(variant_ratio*df.shape[0])
-                variants = df.iloc[num_train_items+1:, :][VARIANT_NAME]
-                labels = df.iloc[num_train_items+1:, :][LABEL]
-                label_index = num_train_items+1
+                num_train_items = int(variant_ratio * df.shape[0])
+                variants = df.iloc[num_train_items + 1:, :][VARIANT_NAME]
+                labels = df.iloc[num_train_items + 1:, :][LABEL]
+                label_index = num_train_items + 1
 
             for v in variants:
                 if y_pred[predict_index] == 0:
@@ -153,10 +153,39 @@ def accuray_measure(y_test, y_pred):
     print("tp_correct", TP_correct)
     print("fp_count", FP_count)
     print("fp_correct", FP_correct)
-    print("tp_precision", (TP_correct)/(TP_correct + FP_count - FP_correct))
-    print("fp_precision", (FP_correct)/(FP_correct + TP_count - TP_correct))
-    print("tp_recall", (TP_correct)/(TP_count))
-    print("fp_recall", (FP_correct)/(FP_count))
+    print("tp_precision", (TP_correct) / (TP_correct + FP_count - FP_correct))
+    print("fp_precision", (FP_correct) / (FP_correct + TP_count - TP_correct))
+    print("tp_recall", (TP_correct) / (TP_count))
+    print("fp_recall", (FP_correct) / (FP_count))
+
+
+def performance_in_each_projects(testing_systems, classified_file):
+
+    for system in testing_systems:
+        TP_correct = 0
+        FP_correct = 0
+        FP_count = 0
+        TP_count = 0
+        print(system)
+        for project in testing_systems[system]:
+            classified_file_path = join_path(join_path(system, project), classified_file)
+            with open(classified_file_path) as file:
+                csvFile = csv.reader(file)
+                for line in csvFile:
+                    if line[1] == TRUE_PASSING:
+                        TP_count += 1
+                        if line[2] == TRUE_PASSING:
+                            TP_correct += 1
+                    else:
+                        FP_count += 1
+                        if line[2] == FALSE_PASSING:
+                            FP_correct += 1
+        print("tp_precision", (TP_correct) / (TP_correct + FP_count - FP_correct))
+        print("fp_precision", (FP_correct) / (FP_correct + TP_count - TP_correct))
+        print("tp_recall", (TP_correct) / (TP_count))
+        print("fp_recall", (FP_correct) / (FP_count))
+        print("accuracy", (TP_correct + FP_correct)/(TP_count + FP_count))
+    pass
 
 
 if __name__ == "__main__":
@@ -165,35 +194,34 @@ if __name__ == "__main__":
     #                     "/Users/thu-trangnguyen/Documents/Research/SPL/Email/1Bug/4wise/",
     #                     "/Users/thu-trangnguyen/Documents/Research/SPL/ExamDB/1Bug/4wise/",
     #                     "/Users/thu-trangnguyen/Documents/Research/SPL/GPL/1Bug/1wise/"]
-    training_systems = []
+    training_systems = [
+                        ]
     testing_systems = [
 
-                        "/Users/thu-trangnguyen/Documents/Research/SPL/Elevator/2Bug/4wise/",
-                        "/Users/thu-trangnguyen/Documents/Research/SPL/Elevator/3Bug/4wise/",
-
-                        "/Users/thu-trangnguyen/Documents/Research/SPL/ExamDB/2Bug/4wise/",
-                        "/Users/thu-trangnguyen/Documents/Research/SPL/ExamDB/3Bug/4wise/",
+        "/Users/trangnt/Documents/Projects/SPL/BankAccountTP/1Bug/4wise/",
 
 
-                        "/Users/thu-trangnguyen/Documents/Research/SPL/Email/2Bug/4wise/",
-                        "/Users/thu-trangnguyen/Documents/Research/SPL/Email/3Bug/4wise/",
+        "/Users/trangnt/Documents/Projects/SPL/Elevator/1Bug/4wise/",
 
-                        "/Users/thu-trangnguyen/Documents/Research/SPL/BankAccountTP/2Bug/4wise/",
-                        "/Users/thu-trangnguyen/Documents/Research/SPL/BankAccountTP/3Bug/4wise/"
-                        ]
-    # testing_systems = ["/Users/thu-trangnguyen/Documents/Research/SPL/BankAccountTP/1Bug/4wise/",
-    #                    "/Users/thu-trangnguyen/Documents/Research/SPL/BankAccountTP/2Bug/4wise/",
-    #                    "/Users/thu-trangnguyen/Documents/Research/SPL/BankAccountTP/3Bug/4wise/"
-    #                    ]
+
+        "/Users/trangnt/Documents/Projects/SPL/Email/1Bug/4wise/",
+
+
+        "/Users/trangnt/Documents/Projects/SPL/GPL/1Bug/4wise/",
+
+
+        "/Users/trangnt/Documents/Projects/SPL/ZipMe/1Bug/2wise/",
+
+
+        "/Users/trangnt/Documents/Projects/SPL/ExamDB/1Bug/4wise/"]
 
     X_train, X_test, y_train, y_test, test_samples = load_all_data(training_systems, testing_systems, 0.8, 0.0)
-    print(test_samples)
-    clf = svm.SVC(kernel='linear')
+
+    clf = svm.SVC(kernel='rbf', class_weight='balanced')
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
     accuray_measure(y_test, y_pred)
-    print(clf.coef_)
 
-
-    write_classified_result(y_pred, test_samples, 0, classified_all_multiple_bug_file)
+    write_classified_result(y_pred, test_samples, 0, classified_all_testing_file)
+    performance_in_each_projects(test_samples, classified_all_testing_file)
