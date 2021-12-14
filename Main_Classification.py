d24470e55adb12545ebfab60efd30fc4331288ea
ttrangnguyen <nguyenthutrang.uet@gmail.com>
2021-12-15 01:05:10 +0700

    uncomment in classification file

--- Main_Classification.py
+++ Main_Classification.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     system_paths["ExamDB"]["3Bug"] = "/Users/thu-trangnguyen/Documents/Research/SPL/ExamDB/3Bug/4wise/"
 
     label_data(system_paths)
-    # calculate_consistent_testing_values_for_features(system_paths, FIELDS)
-    # do_label_statistics(system_paths)
-    # do_features_statistics(system_paths, FIELDS)
-    #
-    # classify_all_cases(system_paths)
-    # classify_single_bug_cases(system_paths)
-    # classify_by_variants(system_paths)
-    # classify_by_systems(system_paths)
-    # classify_by_only_a_system(system_paths)
+    calculate_consistent_testing_values_for_features(system_paths, FIELDS)
+    do_label_statistics(system_paths)
+    do_features_statistics(system_paths, FIELDS)
+
+    classify_all_cases(system_paths)
+    classify_single_bug_cases(system_paths)
+    classify_by_variants(system_paths)
+    classify_by_systems(system_paths)
+    classify_by_only_a_system(system_paths)
